8c98d7399f957168d771a949d4425b863e421203
John Carpenter <122576866+johncarpenter819@users.noreply.github.com>
2025-03-04 14:30:51 -0500

    Finished Project

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-let color = "black"
+let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+    document.querySelector("body").addEventListener("click", function(e){
+        if (e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "Now you can draw";
+            }
+            else{
+                draw.innerHTML = "You're not allowed to draw"
+            }
+        }
+    })
     
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
@@ -44,12 +57,17 @@ function getSize(){
 }
 
 function colorDiv(){
+    if(click){
+
+    
     if(color == "random"){
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
     }
     else{
         this.style.backgroundColor = 'black'
     }
+    }
+
 }
 
 function setColor(colorChoice){
