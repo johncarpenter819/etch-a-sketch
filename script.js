0bd5897b70559b400e0803d233e51b362d48b1ff
John Carpenter <122576866+johncarpenter819@users.noreply.github.com>
2025-03-04 10:53:24 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const containerDiv = document.querySelector("#container");
+
+function makeRows (rows, columns) {
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid columns", columns);
+    console.log(containerDiv);
+    console.log(rows);
+    console.log(columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let square = document.createElement("div");
+        square.innerText = (i + 1);
+        containerDiv.appendChild(square).className = "grid-item";
+    }
+}
+
+makeRows(16, 16);
